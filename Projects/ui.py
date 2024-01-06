2a320078dbaad1c262db358b69c244b6a2cc97a5
wufukevin <wufukevin@gmail.com>
2024-01-06 16:57:25 +0800

    put return button with place()

--- Projects/ui.py
+++ Projects/ui.py
@@ -2,19 +2,14 @@ import tkinter as tk
 
 
 class BaseApp:
-    def __init__(self, parent_frame, return_callback, height):
+    def __init__(self, parent_frame, return_callback):
         self.parent_frame = parent_frame
         self.return_callback = return_callback
-
-        self.height = height
-
         self.create_widgets()
 
     def create_widgets(self):
         # Return to Homepage Button
         self.return_button = tk.Button(
-            self.parent_frame, text="Return to Homepage", command=self.return_callback)
-        # self.return_button.pack(pady=10)
-        print(f"height: {self.height}")
-        self.return_button.place(x=0, y=self.height)
+            self.parent_frame, text="Return", command=self.return_callback)
+        self.return_button.place(relx=0.1, rely=0.9, anchor=tk.CENTER)
         return None
